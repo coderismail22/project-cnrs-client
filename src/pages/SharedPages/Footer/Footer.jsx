8e3c7733f814c89a7dc2b686c30bc01dcc2cfef6
ismail <coderismail22@gmail.com>
2024-08-24 20:04:56 +0600

    make header and footer

--- src/pages/SharedPages/Footer/Footer.jsx
+++ src/pages/SharedPages/Footer/Footer.jsx
@@ -1,5 +1,47 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="p-10 md:p-15 lg:p-20">
+      <div className="lg:flex gap-10 justify-between">
+        <div className="max-w-[1100px] mb-10">
+          <h1 className="font-bold text-xl">
+            Center for Natural Resource Studies (CNRS)
+          </h1>
+          <div>
+            <p>
+              <img
+                src="/src/assets/cnrs.png"
+                alt="CNRS Logo"
+                className="float-left w-16 h-16 mr-4"
+              />
+            </p>
+            <p>
+              CNRS is one of the most recognized pro-environmental national NGOs
+              in Bangladesh. It was conceptualized in 1993, from Bangladesh
+              Flood Action Plan (FAP16) Environmental Study; and the formal
+              journey started in 1994. Since formation, CNRS has been active in
+              this field and has drawn attention of relevant local, national and
+              international actors including government and funding agencies
+              through demonstrating innovations in its approaches and producing
+              tangible learning outcomes. On behalf of its General Committee, an
+              Executive Committee (EC) governs CNRS. CNRS aims to join with
+              others in influencing the national development strategy towards
+              directions that support sustainable environment in Bangladesh.
+            </p>
+          </div>
+        </div>
+        <div className="min-w-56">
+          <h1 className="font-bold">Our Office Address:</h1>
+          <p>
+            House 13, Level 4-6, <br />
+            Road 17, Block D, Banani <br />
+            Dhaka 1213, Bangladesh <br />
+          </p>
+          <p className="mt-10">Tel: 880-2-222274127-8</p>
+        </div>
+      </div>
+      <h1 className="flex-1 mt-5">Copyright © 2024 CNRS | Developed by CNRS</h1>
+    </div>
+  );
 };
 
 export default Footer;
--- src/pages/SharedPages/Navbar/Navbar.jsx
+++ src/pages/SharedPages/Navbar/Navbar.jsx
@@ -1,70 +1,79 @@
-// import { useState } from "react";
-// import { Link } from "react-router-dom";
+import clsx from "clsx";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
-// const navitems = [
-//   { title: "Home", path: "/" },
-//   { title: "Services", path: "#services" },
-//   { title: "Pricing", path: "#pricing" },
-// ];
+const navitems = [
+  { title: "Home", path: "/" },
+  { title: "About", path: "/about" },
+  { title: "What We Do", path: "/what-we-do" },
+  { title: "Our Services", path: "/services" },
+  { title: "Projects", path: "/projects" },
+  { title: "Stories", path: "/stories" },
+  { title: "Publications", path: "/publications" },
+  { title: "Contact", path: "/contact" },
+  { title: "Join Us", path: "/join-us" },
+];
 
-// const Navbar = () => {
-//   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+const Navbar = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-//   return (
-//     <main className="w-full h-[65px] sticky top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 ">
-//       <nav className="flex justify-around items-center px-8 py-3">
-//         <section className="flex items-center gap-4">
-//           {/* MENU */}
-//           <IoMdMenu
-//             className="text-3xl cursor-pointer md:hidden"
-//             onClick={() => setIsSidebarOpen(true)}
-//           />
-//           {/* LOGO */}
-//           <Link href={"/"} className="text-4xl ">
-//             Logo
-//           </Link>
-//         </section>
+  return (
+    <main className="w-full h-[100px] sticky top-0 shadow-lg ">
+      <nav className="flex justify-around items-center px-8 py-3">
+        <section className="flex items-center gap-4">
+          {/* MENU */}
+          <h1
+            className="text-3xl cursor-pointer md:hidden"
+            onClick={() => setIsSidebarOpen(true)}
+          >
+            Open
+          </h1>
+          {/* LOGO */}
+          <img src="/src/assets/cnrs.png" width="80px" />
+        </section>
 
-//         <section className="flex gap-5 ">
-//           {/* Navbar For Larger Displays */}
-//           {navitems.map((item, index) => (
-//             <Link
-//               key={index}
-//               href={item.path}
-//               className="hidden md:block text-2xl font-bold "
-//             >
-//               {item.title}
-//             </Link> 
-//           ))}
-//         </section>
+        <section className="flex gap-5 ">
+          {/* Navbar For Larger Displays */}
+          {navitems.map((item, index) => (
+            <Link
+              key={index}
+              to={item.path}
+              className="hidden md:block text-2xl font-bold "
+            >
+              {item.title}
+            </Link>
+          ))}
+        </section>
 
-//         {/* Mobile Sidebar */}
-//         <div
-//           className={clsx(
-//             "fixed h-full w-screen md:hidden bg-black/50  backdrop-blur-sm top-0 right-0  -translate-x-full  transition-all ",
-//             isSidebarOpen && "translate-x-0"
-//           )}
-//         >
-//           <section className="text-white bg-black/90 h-screen w-56 absolute left-0 top-0 flex flex-col gap-8 p-10 ">
-//             <IoCloseSharp
-//               onClick={() => setIsSidebarOpen(false)}
-//               className="text-3xl cursor-pointer"
-//             />
+        {/* Mobile Sidebar */}
+        <div
+          className={clsx(
+            "fixed h-full w-screen md:hidden bg-black/50  backdrop-blur-sm top-0 right-0  -translate-x-full  transition-all ",
+            isSidebarOpen && "translate-x-0"
+          )}
+        >
+          <section className="text-white bg-black/90 h-screen w-56 absolute left-0 top-0 flex flex-col gap-8 p-10 ">
+            <h1
+              onClick={() => setIsSidebarOpen(false)}
+              className="text-3xl cursor-pointer"
+            >
+              Close
+            </h1>
 
-//             {navitems.map((item, index) => (
-//               <Link key={index} href={item.path} className="font-bold">
-//                 {item.title}
-//               </Link>
-//             ))}
-//           </section>
-//         </div>
-//         <section className="flex gap-4">
-//           {/* Social Media Icons */}
-//           {/* <IoCart className="text-3xl" /> */}
-//         </section>
-//       </nav>
-//     </main>
-//   );
-// };
+            {navitems.map((item, index) => (
+              <Link key={index} to={item.path} className="font-bold">
+                {item.title}
+              </Link>
+            ))}
+          </section>
+        </div>
+        <section className="flex gap-4">
+          {/* Social Media Icons */}
+          {/* <IoCart className="text-3xl" /> */}
+        </section>
+      </nav>
+    </main>
+  );
+};
 
-// export default Navbar;
+export default Navbar;
