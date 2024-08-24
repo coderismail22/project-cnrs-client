94fd11e6ce55821c29cbc1646942b91a360004e5
ismail <coderismail22@gmail.com>
2024-08-24 22:18:28 +0600

    add blinker font

--- src/pages/Home/HeroSection/HeroSection.jsx
+++ src/pages/Home/HeroSection/HeroSection.jsx
@@ -1,29 +1,39 @@
 const HeroSection = () => {
   return (
     // TODO: Fix Text Positioning
-    <div className="relative">
-      <img
-        src="/src/assets/hero-section-img.jpg"
-        alt="CNRS Logo"
-        className="object-cover object-center w-full h-auto"
-      />
-      <div className="absolute top-10 right-4">
-        <p className="text-right text-[#f7f6f4] text-3xl md:4xl lg:text-5xl font-extrabold uppercase  p-4 rounded-lg">
-          Striving <br /> for building <br /> social-ecological <br />
-          resilience
-        </p>
+    <>
+      {/* Hero Image */}
+      <div className="relative">
+        <img
+          src="/src/assets/hero-section-img.jpg"
+          alt="CNRS Logo"
+          className="object-cover object-center w-full h-auto"
+        />
+        <div className="absolute top-10 right-4">
+          <p className="text-right text-[#f7f6f4] text-3xl md:4xl lg:text-5xl font-extrabold uppercase  p-4 rounded-lg">
+            Striving <br /> for building <br /> social-ecological <br />
+            resilience
+          </p>
 
-        <p className="text-right text-[#f7f6f4] text-sm font-extrabold uppercase  p-4 rounded-lg">
-          <span className=" bg-[#3A5231] bg-opacity-90">
-            we work to find solutions to most pressing environmental
-          </span>
-          <br />
-          <span className=" bg-[#3A5231] bg-opacity-90">
-            ecological, livelihood and development challenges.
-          </span>
-        </p>
+          <p className="text-right text-[#f7f6f4] text-sm font-extrabold uppercase  p-4 rounded-lg">
+            <span className=" bg-[#3A5231] bg-opacity-90">
+              we work to find solutions to most pressing environmental
+            </span>
+            <br />
+            <span className=" bg-[#3A5231] bg-opacity-90">
+              ecological, livelihood and development challenges.
+            </span>
+          </p>
+        </div>
       </div>
-    </div>
+      {/* Hero Text */}
+      <div>
+        <h1 className="font-blinker font-extrabold ">
+          we work to find solutions to most pressing environmental, ecological,
+          livelihood and development challenges.
+        </h1>
+      </div>
+    </>
   );
 };
 
