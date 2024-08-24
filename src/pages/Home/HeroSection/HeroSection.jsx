ee458a0c26b0161ee210444f4ee1d18c7b780bb9
ismail <coderismail22@gmail.com>
2024-08-24 22:01:59 +0600

    fix hero section position

--- src/pages/Home/HeroSection/HeroSection.jsx
+++ src/pages/Home/HeroSection/HeroSection.jsx
@@ -7,14 +7,13 @@ const HeroSection = () => {
         alt="CNRS Logo"
         className="object-cover object-center w-full h-auto"
       />
-      <div className="absolute top-28 right-4">
+      <div className="absolute top-10 right-4">
         <p className="text-right text-[#f7f6f4] text-3xl md:4xl lg:text-5xl font-extrabold uppercase  p-4 rounded-lg">
           Striving <br /> for building <br /> social-ecological <br />
           resilience
         </p>
-      </div>
-      <div className="absolute bottom-24 right-4 ">
-        <p className=" text-right text-[#f7f6f4] text-sm font-extrabold uppercase  p-4 rounded-lg">
+
+        <p className="text-right text-[#f7f6f4] text-sm font-extrabold uppercase  p-4 rounded-lg">
           <span className=" bg-[#3A5231] bg-opacity-90">
             we work to find solutions to most pressing environmental
           </span>
