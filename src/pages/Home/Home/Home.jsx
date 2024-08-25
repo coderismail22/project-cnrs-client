5f5593748d1721ac25cfa21909e9821acd439888
ismail <coderismail22@gmail.com>
2024-08-25 09:52:05 +0600

    install react-animated-numbers

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,14 +1,17 @@
 // import HeroSection from "../HeroSection/HeroSection";
 // import HomeCards from "../HomeCards/HomeCards";
 
-import Documentaries from "../Documentaries/Documentaries";
+import Counter from "../../../components/Counter/Counter";
+
+// import Documentaries from "../Documentaries/Documentaries";
 
 const Home = () => {
   return (
     <div>
       {/* <HeroSection />
       <HomeCards /> */}
-      <Documentaries />
+      {/* <Documentaries /> */}
+      <Counter />
     </div>
   );
 };
