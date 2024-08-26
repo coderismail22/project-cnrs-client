4337c9d3c1ee8dfc392446092a0d4a4a643de0d3
ismail <coderismail22@gmail.com>
2024-08-26 13:46:38 +0600

    make membership component

--- src/pages/About/About/About.jsx
+++ src/pages/About/About/About.jsx
@@ -1,13 +1,13 @@
 import CounterForAboutPage from "../../../components/CounterForAboutPage/CounterForAboutPage";
 import Profile from "../../../components/Profile/Profile";
+import Membership from "../Membership/Membership";
 
 const About = () => {
   return (
     <div>
-      {/* Profile */}
       {/* <Profile /> */}
-      {/* Counter */}
-      <CounterForAboutPage />
+      {/* <CounterForAboutPage /> */}
+      {/* <Membership /> */}
     </div>
   );
 };
---
+++ src/pages/About/Membership/Membership.jsx
@@ -0,0 +1,37 @@
+const Membership = () => {
+  return (
+    <div className="text-[#001848] p-10 max-w-[900px]">
+      <h1 className="font-palanquin font-extrabold text-4xl my-5 ">
+        Organizational Membership
+      </h1>
+      <div className="font-lato text-xl">
+        <p className="my-5">
+          CNRS has partnership and membership with some Forums and Associations.
+          Among them the followings are worth mentioning:
+        </p>
+        <ul className="list-disc p-5">
+          <li>
+            CSO-SUN- a network of civil society organizations for scaling up
+            nutrition
+          </li>
+          <li>IUCN Bangladesh National Committee</li>
+          <li>PRA Promoters&apos; Society-Bangladesh (PPS-BD)</li>
+          <li>
+            CSO-SUN- a network of civil society organizations for scaling up
+            nutrition
+          </li>
+          <li>Coastal Fisher-folk Community Network (COFCON)</li>
+          <li>Credit Development Forum (CDF)</li>
+          <li>Asian DRR network</li>
+          <li>Bangladesh Network for Environmental Governance (BNEG)</li>
+          <li>CNRS runs a CBO network (community institutions)</li>
+          <li>
+            Secretariat of HAP (Haor Advocacy Platform)- a civil society network
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Membership;
