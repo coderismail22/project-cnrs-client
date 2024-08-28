8190756e19bab39196e77c341c6324274e4b50b0
ismail <coderismail22@gmail.com>
2024-08-28 16:17:27 +0600

    make pagetitlewithimage compo

--- src/pages/About/About/About.jsx
+++ src/pages/About/About/About.jsx
@@ -2,10 +2,15 @@ import CounterForAboutPage from "../../../components/CounterForAboutPage/Counter
 import Profile from "../../../components/Profile/Profile";
 import Membership from "../Membership/Membership";
 import KeyPartners from "../KeyPartners/KeyPartners";
+import PageTitleWithImage from "../../../components/PageTitleWithImage/PageTitleWithImage";
 
 const About = () => {
   return (
     <div>
+      <PageTitleWithImage
+        pageName={"About Us"}
+        bgImage={"/assets/hero-img.jpg"}
+      />
       <Profile />
       <CounterForAboutPage />
       <Membership />
